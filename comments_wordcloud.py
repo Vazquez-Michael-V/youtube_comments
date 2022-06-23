c06fbc1b329b3275eadcd5f48bdfe6d53e38ee74
Michael Vazquez <75464767+Vazquez-Michael-V@users.noreply.github.com>
2022-06-22 23:29:06 -0400

    Updates the text filename.

--- comments_wordcloud.py
+++ comments_wordcloud.py
@@ -6,7 +6,7 @@ from wordcloud import WordCloud, STOPWORDS
 from datetime import datetime
 
 # Load the words.
-with open('all_comments_Zzpbbspsuak.txt', encoding='UTF-8') as f:
+with open('top_level_comments_Zzpbbspsuak.txt', encoding='UTF-8') as f:
     text = f.read()
     
 
